fe1a12ed00a8fad044271b5dc74d9855ab44ff0a
TeaIceLemon <piotr.wieczorek1993@gmail.com>
2019-10-20 15:07:39 +0200

    Added HomePageController;

---
+++ src/main/java/pl/TeaIceLemon/SpringIntro/SpringIntro/controllers/HomePageController.java
@@ -0,0 +1,28 @@
+package pl.TeaIceLemon.SpringIntro.SpringIntro.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.TeaIceLemon.SpringIntro.SpringIntro.model.entity.Advert;
+import pl.TeaIceLemon.SpringIntro.SpringIntro.repositories.AdvertRepository;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class HomePageController {
+    private final AdvertRepository advertRepository;
+
+    @Autowired
+    public HomePageController(AdvertRepository advertRepository) {
+        this.advertRepository = advertRepository;
+    }
+
+    @GetMapping
+    public String prepareHomePage(Model model){
+        List<Advert> advertList = advertRepository.findAllByOrderByPostedDesc();
+        return "";
+    }
+}
--- src/main/java/pl/TeaIceLemon/SpringIntro/SpringIntro/repositories/AdvertRepository.java
+++ src/main/java/pl/TeaIceLemon/SpringIntro/SpringIntro/repositories/AdvertRepository.java
@@ -3,5 +3,8 @@ package pl.TeaIceLemon.SpringIntro.SpringIntro.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.TeaIceLemon.SpringIntro.SpringIntro.model.entity.Advert;
 
+import java.util.List;
+
 public interface AdvertRepository extends JpaRepository<Advert, Long> {
+    List<Advert> findAllByOrderByPostedDesc();
 }
